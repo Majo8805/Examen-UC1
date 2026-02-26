f8802cb00eb53ddc331544d6332d7ed34ddd2bbc
Majo8805 <maria.enriquez252337@potros.itson.edu.mx>
2026-02-26 14:51:26 -0700

    Creación de proyecto

---
+++ CU_PagarLuz/src/main/java/com/mycompany/cu_pagarluz/CU_PagarLuz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cu_pagarluz;
+
+/**
+ *
+ * @author garfi
+ */
+public class CU_PagarLuz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
