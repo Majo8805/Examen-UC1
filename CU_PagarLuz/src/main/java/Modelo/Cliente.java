b1f43d4bdeac3b1bebdfe8fe848ed2682a76cb4c
Majo8805 <maria.enriquez252337@potros.itson.edu.mx>
2026-02-26 17:15:20 -0700

    Creación de entidad Cliente

--- CU_PagarLuz/src/main/java/Modelo/Cliente.java
+++ CU_PagarLuz/src/main/java/Modelo/Cliente.java
@@ -10,4 +10,53 @@ package Modelo;
  */
 public class Cliente {
     
+    private String idCliente;
+    private String nombre;
+    private float kwh;
+    private double total;
+
+    public Cliente(String idCliente, String nombre, float kwh, double total) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.kwh = kwh;
+        this.total = total;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getKwh() {
+        return kwh;
+    }
+
+    public void setKwh(float kwh) {
+        this.kwh = kwh;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "idCliente=" + idCliente + ", nombre=" + nombre + ", kwh=" + kwh + ", total=" + total + '}';
+    }
+    
 }
